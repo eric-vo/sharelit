02bfd491c68f692f46c63b06590319a7060afbf4
Eric Vo <eric.aq.vo@gmail.com>
2023-08-21 00:34:18 -0700

    Add location and delete option

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from datetime import datetime
 from string import ascii_lowercase, digits
 
 from cs50 import SQL
-from flask import (Flask, flash, jsonify, redirect, render_template, request,
-                   session)
+from flask import (
+    Flask,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    session,
+)
 from flask_socketio import SocketIO, emit
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -13,8 +20,8 @@ from helpers import login_required
 VALID_USERNAME_CHARS = ascii_lowercase + digits + "_-."
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode='threading')
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app, async_mode="threading")
 
 # Configure session
 app.config["SESSION_PERMANENT"] = False
@@ -23,7 +30,6 @@ Session(app)
 
 db = SQL("sqlite:///sharelit.db")
 
-chat_messages = [] # List of messages
 
 @app.route("/")
 def index():
@@ -63,15 +69,29 @@ def create():
     return render_template("create.html", username=get_username(session))
 
 
-@app.route("/listings")
-@login_required
+@app.route("/listings", methods=["GET", "POST"])
 def listings():
     listings = get_books()
     # Get names of users who created listings using JOIN
     listing_users = db.execute(
-        "SELECT username FROM users JOIN listings ON "
+        "SELECT users.id, username FROM users JOIN listings ON "
         "users.id = listings.user_id"
     )
+
+    if request.method == "POST":
+        # Get form data
+        listing_id = request.form.get("listing-id")
+        creator_id = request.form.get("creator-id")
+
+        # Ensure person deleting listing is the creator
+        if session["user_id"] != int(creator_id):
+            flash("You can only delete your own listings.")
+            return redirect("/listings")
+
+        # Delete listing
+        db.execute("DELETE FROM listings WHERE id = ?", listing_id)
+        flash("Deleted listing!")
+        return redirect("/listings")
 
     return render_template(
         "listings.html",
@@ -85,16 +105,25 @@ def listings():
 def listing(listing_id):
     listing = db.execute("SELECT * FROM listings WHERE id = ?", listing_id)
     listing_user = db.execute(
-    "SELECT users.username, users.id FROM users JOIN listings ON "
-    "users.id = listings.user_id WHERE listings.id = ?",
-    listing_id,
+        "SELECT users.username, users.id FROM users JOIN listings ON "
+        "users.id = listings.user_id WHERE listings.id = ?",
+        listing_id,
     )
-
 
     if request.method == "POST":
         # Get form data
         duration = request.form.get("duration")
         time_period = request.form.get("time-period")
+
+        # Ensure user requesting is not the creator
+        if session["user_id"] == listing_user[0]["id"]:
+            flash("You cannot request your own listing.")
+            return render_template(
+                "listing.html",
+                listing=listing[0],
+                listing_user=listing_user[0],
+                username=get_username(session),
+            )
 
         # Ensure all fields are filled
         if not (duration and time_period):
@@ -108,7 +137,9 @@ def listing(listing_id):
 
         # Insert into requests table (replace with actual table and columns)
         db.execute(
-            "INSERT INTO requests (user_id, listing_id, duration, time_period) VALUES (?, ?, ?, ?)",
+            "INSERT INTO requests "
+            "(user_id, listing_id, duration, time_period) "
+            "VALUES (?, ?, ?, ?)",
             session["user_id"],
             listing_id,
             duration,
@@ -116,9 +147,16 @@ def listing(listing_id):
         )
 
         # Message the user from current user about the request
-        message = "I would like to borrow your book for " + duration + " " + time_period + "."
+        message = (
+            "I would like to borrow your book for "
+            + duration
+            + " "
+            + time_period
+            + "."
+        )
         db.execute(
-            "INSERT INTO messages (from_user_id, to_user_id, message) VALUES (?, ?, ?)",
+            "INSERT INTO messages "
+            "(from_user_id, to_user_id, message) VALUES (?, ?, ?)",
             session["user_id"],
             listing_user[0]["id"],
             message,
@@ -152,7 +190,11 @@ def chat():
 @login_required
 def messages():
     conversations = get_user_conversations(session)
-    return render_template("messages.html", conversations=conversations, username=get_username(session))
+    return render_template(
+        "messages.html",
+        conversations=conversations,
+        username=get_username(session),
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -218,13 +260,19 @@ def register():
         # Get form data
         username = request.form.get("username")
         full_name = request.form.get("full-name")
+        city_state_country = request.form.get("city-state-country")
         email = request.form.get("email")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
 
         # Ensure all fields are filled
         if not (
-            username and full_name and email and password and confirmation
+            username
+            and full_name
+            and city_state_country
+            and email
+            and password
+            and confirmation
         ):
             flash("All fields are required.")
             return render_template("register.html")
@@ -262,10 +310,12 @@ def register():
 
         # Insert user into database
         user_id = db.execute(
-            "INSERT INTO users (username, full_name, email, hash) "
-            "VALUES (?, ?, ?, ?)",
+            "INSERT INTO users "
+            "(username, full_name, city_state_country, email, hash) "
+            "VALUES (?, ?, ?, ?, ?)",
             username,
             full_name,
+            city_state_country,
             email,
             generate_password_hash(password),
         )
@@ -287,11 +337,24 @@ def handle_send_message(data):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = data["message"]
 
-    # Insert message into the database (update with the actual table and columns)
-    db.execute("INSERT INTO messages (from_user_id, to_user_id, message, timestamp) VALUES (?, ?, ?, ?)", from_user_id, to_user_id, message, timestamp)
+    # Insert message into the database
+    # (update with the actual table and columns)
+    db.execute(
+        "INSERT INTO messages "
+        "(from_user_id, to_user_id, message, timestamp) VALUES (?, ?, ?, ?)",
+        from_user_id,
+        to_user_id,
+        message,
+        timestamp,
+    )
 
     # Create a message object for broadcasting
-    message = {"message": message, "timestamp": timestamp, "from_user_id": from_user_id, "to_user_id": to_user_id}
+    message = {
+        "message": message,
+        "timestamp": timestamp,
+        "from_user_id": from_user_id,
+        "to_user_id": to_user_id,
+    }
 
     # Emit the message to all connected clients
     socketio.emit("receive_message", message)
@@ -299,7 +362,11 @@ def handle_send_message(data):
 
 @app.route("/get_messages")
 def get_messages():
-    messages = db.execute("SELECT * FROM messages WHERE from_user_id = ? OR to_user_id = ?", session["user_id"], session["user_id"])
+    messages = db.execute(
+        "SELECT * FROM messages WHERE from_user_id = ? OR to_user_id = ?",
+        session["user_id"],
+        session["user_id"],
+    )
     return jsonify(messages)
 
 
@@ -327,31 +394,42 @@ def get_books():
 
     return books
 
+
 def get_user_conversations(session):
     messages = db.execute(
-        "SELECT * FROM messages WHERE from_user_id = ? OR to_user_id = ?", session["user_id"], session["user_id"]
+        "SELECT * FROM messages WHERE from_user_id = ? OR to_user_id = ?",
+        session["user_id"],
+        session["user_id"],
     )
 
     conversations = {}
     for message in messages:
-        other_user_id = message["from_user_id"] if message["from_user_id"] != session["user_id"] else message["to_user_id"]
-        other_user = db.execute("SELECT username FROM users WHERE id = ?", other_user_id)[0]
+        other_user_id = (
+            message["from_user_id"]
+            if message["from_user_id"] != session["user_id"]
+            else message["to_user_id"]
+        )
+        other_user = db.execute(
+            "SELECT username FROM users WHERE id = ?", other_user_id
+        )[0]
 
         # Create a conversation entry if it doesn't exist
         if other_user_id not in conversations:
             conversations[other_user_id] = {
-                'id': other_user_id,
-                'name': other_user['username'],
-                'messages': []
+                "id": other_user_id,
+                "name": other_user["username"],
+                "messages": [],
             }
 
         # Append the message to the conversation's messages list
-        conversations[other_user_id]['messages'].append({
-            'text': message['message'],
-            'timestamp': message['timestamp'],
-            'from_user_id': message['from_user_id'],
-            'to_user_id': message['to_user_id']
-        })
+        conversations[other_user_id]["messages"].append(
+            {
+                "text": message["message"],
+                "timestamp": message["timestamp"],
+                "from_user_id": message["from_user_id"],
+                "to_user_id": message["to_user_id"],
+            }
+        )
 
     # Convert the conversations dictionary to a list
     conversations_list = list(conversations.values())
@@ -371,10 +449,11 @@ def handle_request_conversation(data):
         formatted_message = {
             "message": message["message"],
             "timestamp": message["timestamp"],
-            "username": get_username(message["user_id"])
+            "username": get_username(message["user_id"]),
         }
         emit("receive_message", formatted_message)
-    
+
+
 if __name__ == "__main__":
     socketio.run(app)
     print("Running app.py")
